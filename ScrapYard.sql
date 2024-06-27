26cc6a032e9a771293575bb72fb464fd18391c4e
kifidon <113075637+kifidon@users.noreply.github.com>
2024-06-27 14:38:09 -0600

    Edits

--- ScrapYard.sql
+++ ScrapYard.sql
@@ -1,1 +1,22 @@
 -- For Testing, debugging, and Misc. Sql Operations
+
+
+
+select * from AttendanceApproved att where att.name like 'Adrienne Belle Alexis' and att.[date] = '2024-06-19'
+
+SELECT att.name, att.Date, att.RegularHrs, att.Overtime, att.TotalHours , att.TimeOff, att.policy_name, att.Holiday  FROM AttendanceApproved att
+            WHERE att.Date BETWEEN '2024-06-16' AND '2024-06-22'
+
+            Union ALL
+
+            Select tt.name,Null, Sum(tt.RegularHrs), Sum(tt.Overtime), Sum(tt.TotalHours), Sum(tt.TimeOff), 'Policy_name', 'Holiday' From AttendanceApproved tt
+            WHERE [Date] BETWEEN '2024-06-16' AND '2024-06-22'
+            Group By tt.name
+
+            ORDER BY [name], Date DESC
+
+            Select * from Lem
+
+
+SELECT table_name
+FROM information_schema.tables
--- lemDef.sql
+++ lemDef.sql
@@ -1,93 +1,119 @@
-workspace_id VARCHAR(50),
-FOREIGN KEY (workspace_id) REFERENCES Workspace(id)
 
 
-create TABLE lemEntry (
-    id VARCHAR(50),
-    lemid VARCHAR(50),
-    PRIMARY KEY (id, workspace_id),
-    IsForWorker VARCHAR(100),
-    work DECIMAL(10,2),
-    travel DECIMAL(10,2),
-    Calc DECIMAL(10,2),
-    Meals DECIMAL(10,2),
-    Hotel DECIMAL(10,2)
-    FOREIGN KEY (workspace_id) REFERENCES Workspace(id) ON DELETE CASCADE 
-    -- Add other lemEntry columns as needed
-);
--- Lemworker Table
-
-create TABLE lemWorker (
-    EmpId VARCHAR(50),
-    roleid VARCHAR(50),
-    PRIMARY KEY (id, workspace_id),
+Drop Table If Exists Role
+CREATE TABLE Role(
+    id VARCHAR(50) Primary KEY, 
+    [name] NVARCHAR (50), 
 );
 
--- EquipEntry Table
-
-CREATE TABLE EquipEntry (
-    id VARCHAR(50),
-    lemid VARCHAR(50),
-    PRIMARY KEY (id, workspace_id),
-    equipid VARCHAR (50),
-    is_unitrate BIT default 0,
-    Qty DECIMAL (10, 2),
-    FOREIGN KEY (workspace_id) REFERENCES Workspace(id) ON DELETE CASCADE 
-    FOREIGN KEY (lemid) REFERENCES lemEntry(lemid)
+-- clientRep Table
+Drop Table If Exists ClientRep
+CREATE TABLE ClientRep(
+    empId VARCHAR(50),
+    clientId VARCHAR(50) Unique, 
+    workspaceId Varchar(50),
+    PRIMARY KEY (empId, workspaceId),
+    FOREIGN KEY (clientId ,workspaceId) REFERENCES Client(id, workspace_id) ON DELETE CASCADE,
+    FOREIGN KEY (empId) REFERENCES EmployeeUser(id) 
 );
-
--- Equipment Table
-
-CREATE TABLE Equipment(
-    id VARCHAR(50),
-    Eqp_name VARCHAR(50),
-    PRIMARY KEY (id, workspace_id),
-);
-
 -- Lemsheet Table
 
+
+Drop Table If Exists LemSheet
 CREATE TABLE LemSheet(
     id VARCHAR(50),
-    Clientid VARCHAR(50),
-    PRIMARY KEY (id, workspace_id),
+    clientId VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (id, workspaceId),
     lem_sheet_date DATE,
-    Discripation VARCHAR(50),
-    notes bigint,
-    projectid VARCHAR(50),
-    projectmanagerid bigint,
-    FOREIGN KEY (Clientid_id) REFERENCES Client(id) ON DELETE CASCADE 
+    discripation VARCHAR(50),
+    notes NVARCHAR(250),
+    projectId VARCHAR(50),
+    projectManagerId VARCHAR(50),
+    FOREIGN KEY (clientId, workspaceId) REFERENCES Client(id, workspace_id) ON DELETE CASCADE ,
+    FOREIGN key(projectId, workspaceId) References Project(id, workspace_id) ,
+    FOREIGN key(workspaceId) References Workspace(id) ,
+    FOREIGN key(projectManagerId) References EmployeeUser(id) 
 );
--- clientRep Table
 
-CREATE TABLE ClientRep(
-    empid VARCHAR(50),
-    Clientid VARCHAR(50),
-    PRIMARY KEY (empid, workspace_id),
-    FOREIGN KEY (Clientid_id) REFERENCES Client(id) ON DELETE CASCADE
+Drop Table If Exists LemWorker
+create TABLE LemWorker (
+    empId VARCHAR(50),
+    roleId VARCHAR(50),
+    workspaceId Varchar(50),
+    Unique (empId, roleId, workspaceId),
+    PRIMARY KEY (empId,  workspaceId),
+    FOREIGN KEY (workspaceId) REFERENCES Workspace(id) ,
+    FOREIGN KEY (empId) REFERENCES EmployeeUser(id) On Delete CASCADE,
+    FOREIGN KEY (roleId) REFERENCES [Role](id) 
 );
--- EqpRateSheet Table
 
-CREATE TABLE EqpRateSheet(
-    equipid VARCHAR(50),
-    Clientid VARCHAR(50),
-    unitrate DECIMAL(10,2),
-    dayrate DECIMAL(10,2),
-    PRIMARY KEY (id, workspace_id),
-);
--- WorkRateShet Table
 
-CREATE TABLE workRateShet(
-    Clientid VARCHAR(50),
-    roleid VARCHAR(50),
-    workrate DECIMAL(10,2),
-    travelrate DECIMAL(10,2)
-    Calc DECIMAL(10,2),
-    PRIMARY KEY (id, workspace_id),
-);
--- WorkRateShet Table
 
-CREATE TABLE Role(
+-- Equipment Table
+Drop Table If Exists Equipment
+CREATE TABLE Equipment(
     id VARCHAR(50),
-    Role_Name NVARCHAR (50), 
-    PRIMARY KEY (id, workspace_id),
+    [name] VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (id, workspaceId),
+    FOREIGN Key (workspaceId) REFERENCES Workspace(id)
+);
+
+
+-- EquipEntry Table
+Drop Table If Exists EquipEntry
+CREATE TABLE EquipEntry (
+    id VARCHAR(50),
+    lemId VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (id, lemId, workspaceId),
+    equipId VARCHAR (50),
+    isUnitRate BIT default 0,
+    qty DECIMAL (10, 2) Default 0.00,
+    FOREIGN KEY (workspaceId) REFERENCES Workspace(id) , 
+    FOREIGN KEY (lemId, workspaceId) REFERENCES LemSheet(id, workspaceId) On Delete CASCADE,
+    FOREIGN KEY (equipId, workspaceId) REFERENCES Equipment(id, workspaceId) ,
+);
+
+Drop Table If Exists LemEntry
+create TABLE LemEntry (
+    lemId VARCHAR(50),
+    workerId VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (lemId, workerId, workspaceId),
+    work DECIMAL(10,2) Default 0.00,
+    travel DECIMAL(10,2) Default 0.00,
+    Calc DECIMAL(10,2) Default 0.00,
+    Meals DECIMAL(10,2) Default 0.00,
+    Hotel DECIMAL(10,2) Default 0.00,
+    FOREIGN KEY (workspaceId) REFERENCES Workspace(id) ,
+    FOREIGN KEY (lemId, workspaceId) REFERENCES LemSheet(id, workspaceId) ON DELETE CASCADE ,
+    FOREIGN KEY (workerId, workspaceId) REFERENCES LemWorker(empId, workspaceId) ,
+    -- Add other LemEntry columns as needed
+);
+Drop Table if Exists WorkerRateSheet
+CREATE TABLE WorkerRateSheet(
+    clientId VARCHAR(50),
+    roleId VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (clientId, roleId, workspaceId),
+    workRate DECIMAL(10,2),
+    travelRate DECIMAL(10,2),
+    calcRate DECIMAL(10,2),
+    FOREIGN Key (clientId, workspaceId ) References Client(id, workspace_id),
+    FOREIGN Key (roleId) References Role(id),
+    FOREIGN Key (workspaceId) References Workspace(id) ,
+);
+
+Drop Table if Exists EqpRateSheet
+CREATE TABLE EqpRateSheet(
+    equipId VARCHAR(50),
+    clientId VARCHAR(50),
+    workspaceId Varchar(50),
+    PRIMARY KEY (equipid, clientId, workspaceId),
+    unitRate DECIMAL(10,2) Default 0.00,
+    dayRate DECIMAL(10,2) Default 0.00,
+    FOREIGN Key (equipId, workspaceId) References Equipment(id, workspaceId) ,
+    FOREIGN Key (clientId, workspaceId) References Client(id, workspace_id) On Delete Cascade 
 );
