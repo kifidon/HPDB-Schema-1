e8d17057444e58e35d6bafdea636674203aa0ace
akbmah-oss <akbar_aa114@yahoo.com>
2024-06-28 11:41:43 -0700

    Update ScrapYard.sql

--- ScrapYard.sql
+++ ScrapYard.sql
@@ -20,3 +20,24 @@ SELECT att.name, att.Date, att.RegularHrs, att.Overtime, att.TotalHours , att.Ti
 
 SELECT table_name
 FROM information_schema.tables
+
+--- test data
+INSERT INTO Role(id, [name] )
+VALUES
+    ('id', 1099),
+    ('Name', Dixon);
+
+--- test data
+INSERT INTO Equipment (id, [name])
+VALUES
+    ('id', 1099),
+    ('Name', Dixon);
+
+--- test data
+INSERT INTO EqpRateSheet (equipId, clientId,unitRate,dayRate)
+VALUES
+    ('iequipEd', 1099),
+    ('clientId', Dixon),
+    ('unitRate', 34.5),
+    ('dayRate', 21);
+
